3f6d8d2c1c6e481fb7b5e14d3f22dd50b26a506f
ArthurW <arthur@tarides.com>
2023-06-23 16:33:57 +0200

    simplify

--- mycamlbindings.ml
+++ mycamlbindings.ml
@@ -1,35 +1,23 @@
+let rec fib n = if n <= 1 then n else fib (n - 1) + fib (n - 2)
+
 module Stubs (I : Cstubs_inverted.INTERNAL) = struct
   include Ctypes
-
-  type _ Effect.t += Yield : int -> unit Effect.t
 
   let () =
     I.internal ~runtime_lock:false "mycamlbindings_toto"
       (void @-> returning void)
       (fun () ->
         let () =
-          (* Eio_posix.run @@ fun _env -> Format.printf "(test) coucou@." *)
           let open Effect.Deep in
           match_with
-            (fun () -> Effect.perform (Yield 1))
+            (fun () -> ignore (fib 5))
             ()
             {
               retc = (fun x -> x);
               exnc = (fun e -> raise e);
-              effc =
-                (fun (type a) (e : a Effect.t) ->
-                  match e with
-                  | Yield x ->
-                      Some
-                        (fun (k : (a, _) continuation) ->
-                          Format.printf "yield %i@." x;
-                          continue k ())
-                  | _ -> None);
+              effc = (fun (type a) (_ : a Effect.t) -> None);
             };
           ()
         in
-
-        Format.printf "and out!@.";
-
-        ())
+        Format.printf "and out!@.")
 end
